292de61314268db52a70d005250555381eb8254d
Justin Kennedy <kennedjustin7@gmail.com>
2024-09-20 22:24:49 -0400

    ... removing published secret...

--- ClearDatabaseValues.py
+++ ClearDatabaseValues.py
@@ -1,8 +1,13 @@
 from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
 
-url: str = "https://kdubupeizubmgkwgcguu.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkdWJ1cGVpenVibWdrd2djZ3V1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjMyMzg4MCwiZXhwIjoyMDQxODk5ODgwfQ._PMoTOlc2D53dn7Mmr0j5Ka_UoruxVK4TilzpflXd08"
-supabase: Client = create_client(url, key)
+load_dotenv()
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_SERVICE_KEY = os.getenv('SUPABASE_SERVICE_KEY')
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
 supabase.table("eod_equations").delete().neq('eq_data', 0).execute()
 supabase.table("data").delete().neq('value', -1).execute()
